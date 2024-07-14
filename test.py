ba387019ecac67e066faad3967abe13fcb270d46
rajiv <rajiv.rjvcode@gmail.com>
2024-07-14 14:05:51 +0530

    added side frame elements

--- test.py
+++ test.py
@@ -16,12 +16,12 @@ tk.Label(f2, text="Child Frame 2").pack(fill="both", expand=True)
 tk.Label(f3, text="Child Frame 3").pack(fill="both", expand=True)
 
 s1 = ttk.Separator(outer_frame, orient="horizontal")
-# s2 = ttk.Separator(outer_frame, orient="horizontal")
+s2 = ttk.Separator(outer_frame, orient="horizontal")
 
 f1.pack(side="top", fill="both", expand=True)
 s1.pack(side="top", fill="x", expand=False)
 f2.pack(side="top", fill="both", expand=True)
-# s2.pack(side="top", fill="x", expand=False)
+s2.pack(side="top", fill="x", expand=False)
 f3.pack(side="top", fill="both", expand=True)
 
 root.mainloop()
--- ui.py
+++ ui.py
@@ -1,9 +1,12 @@
 
+
+
 from tkinter import *
 from tkinter import ttk
 
 
 def print_okey():
+
     print('clicked')
 
 
@@ -20,18 +23,34 @@ f1.configure('Danger.TFrame', background='snow2', borderwidth=1, relief='raised'
 
 f2 = ttk.Style()
 f2.configure('s.TFrame', background='ivory2', borderwidth=1, relief='raised')
-
+title_window = ttk.Label(window,text = 'TYPING TEST' ,font=('Arial 35 bold'),relief='flat').pack(side='top')
 #frames
-main_frame= ttk.Frame(window,style='Danger.TFrame',width=200,height=200)
-main_frame.pack_propagate(False)
-subframe = ttk.Frame(main_frame,width=250,height=100,style='s.TFrame')
-subframe.pack_propagate(False)
 
-title_frame = ttk.Frame(window,style='Danger.TFrame',width=200,height=200)
+#side frame
+side_frame= ttk.Frame(window, style='Danger.TFrame', width=200, height=200,padding=10)
+side_frame.pack_propagate(False)
+# subframe = ttk.Frame(side_frame, width=250, height=100, style='s.TFrame')
+# subframe.pack_propagate(False)
+start_btn = ttk.Button(side_frame,text='Start').pack(side='top',fill='y')
+
+reset_btn = ttk.Button(side_frame,text='Reset').pack(side='bottom',fill='y')
+
+timer = ttk.Label(side_frame,text='timer',padding=10).pack(side='top')
+timer_value = ttk.Label(side_frame,text='0').pack(side='top')
+wpm = ttk.Label(side_frame,text='words per minute').pack(side='top')
+timer_value = ttk.Label(side_frame,text='0').pack(side='top')
+accuracy = ttk.Label(side_frame,text='accuracy').pack(side='top')
+timer_value = ttk.Label(side_frame,text='0').pack(side='top')
 
 
-operation = ttk.Frame(window,style='Danger.TFrame',width=200,height=200)
+#text frame
+text_frame = ttk.Frame(window,style='Danger.TFrame',width=200,height=200,padding=20)
+text_frame.pack_propagate(False)
+paragraph = ttk.Label(text_frame,text='ram',wraplength=1000,justify='left')
 
+#input frame
+input_frame = ttk.Frame(window, style='Danger.TFrame', width=200, height=200,padding=20)
+entry = Text(input_frame,height=8,width=110)
 #subframes
 
 
@@ -51,17 +70,23 @@ operation = ttk.Frame(window,style='Danger.TFrame',width=200,height=200)
 
 #positioning
 #mainframe position
-main_frame.pack(side='left',expand=False,fill='y')
-subframe.pack()
-ttk.Label(subframe,text='top').pack(side='top',expand=True)
-ttk.Label(subframe,text='center',).pack(side='top',expand=True)
-ttk.Label(subframe,text='bottom',).pack(expand=True)
+side_frame.pack(side='left', expand=False, fill='y')
 
-title_frame.pack(side='top',expand = False,fil='x')
+# subframe.pack()
 
 
-operation.pack(side='bottom',expand = True,fil='both')
 
+#text frame position
+
+
+text_frame.pack(side='top',expand = False,fil='x')
+paragraph.pack()
+
+
+
+#input frame position
+input_frame.pack(side='bottom', expand = True, fil='both')
+entry.pack()
 
 
 
